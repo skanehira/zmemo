7ec65b4c36e4493f7e80653e6a8265e5922235bb
skanehira <sho19921005@gmail.com>
2018-05-27 15:39:24 +0900

    ログ出力設定追加

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jinzhu/configor"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/comail/colog"
 )
 
 func main() {
@@ -20,7 +23,7 @@ func main() {
 
 	// connect db
 	// user:password@tcp(localhost:3306)/dbname
-	db, err := gorm.Open("mysql", config.DB.User+":"+config.DB.Password+"@tcp("+config.DB.Host+":"+config.DB.Port+")/"+config.DB.Name)
+	db, err := gorm.Open("mysql", config.DB.User+":"+config.DB.Password+"@tcp("+config.DB.Host+":"+config.DB.Port+")/"+config.DB.Name+"?"+"charset=utf8mb4,utf8")
 
 	if err != nil {
 		panic(err)
@@ -42,6 +45,24 @@ func main() {
 		}
 		os.Exit(0)
 	}
+
+	// ログ設定
+	// colog.SetDefaultLevel(colog.LDebug)
+	colog.SetMinLevel(colog.LTrace)
+	colog.SetFormatter(&colog.StdFormatter{
+		Colors: true,
+		Flag:   log.Ldate | log.Ltime | log.Lshortfile,
+	})
+	colog.Register()
+
+	// ログの設定方法
+	// log.Printf("trace: this is a trace log.")
+	// log.Printf("debug: this is a debug log.")
+	// log.Printf("info: this is an info log.")
+	// log.Printf("warn: this is a warning log.")
+	// log.Printf("error: this is an error log.")
+	// log.Printf("alert: this is an alert log.")
+	// log.Printf("this is a default level log.")
 
 	// サーバ開始
 	server := Server{config.Port, db}
